b7b71f6a702cf75c6e6d30d1a3e1ee07a4a1893f
joaoHass <joaopedrohass@outlook.com>
2024-04-23 11:07:58 -0300

    refact! change property type from ProcessedFile.cs
    Forgot to commit this one. Has relation with the commit c6857b
    
    See commit c6857b

--- ImageProcessor/Domain/Models/ProcessedFile.cs
+++ ImageProcessor/Domain/Models/ProcessedFile.cs
@@ -4,10 +4,10 @@ public class ProcessedFile
 {
     public string OriginalName { get; private set; }
     public string NewName { get; private set; }
-    public Stream? ConvertedFile { get; set; }
+    public MemoryStream? ConvertedFile { get; set; }
     public FileStatus FileStatus { get; set; }
     
-    public ProcessedFile(string originalName, string newName, Stream? convertedImage, FileStatus fileStatus)
+    public ProcessedFile(string originalName, string newName, MemoryStream? convertedImage, FileStatus fileStatus)
     {
         OriginalName = originalName;
         NewName = newName;
