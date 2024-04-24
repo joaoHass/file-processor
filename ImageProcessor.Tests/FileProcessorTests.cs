1ed5e7c01e2fa5526a452c058cdc93df6a6e1590
joaoHass <joaopedrohass@outlook.com>
2024-04-24 04:16:17 -0300

    test add more tests for FileProcessor.cs

--- ImageProcessor.Tests/FileProcessorTests.cs
+++ ImageProcessor.Tests/FileProcessorTests.cs
@@ -12,17 +12,34 @@ public class FileProcessorTests(ITestOutputHelper testOutputHelper)
     [Fact]
     public async void Invalid_files_should_be_processed_as_failed()
     {
-        var files = new Dictionary<Stream, string> { { new MemoryStream("Test file"u8.ToArray()), "test file name" } };
+        var files = CreateInvalidFile();
         var processor = new FileProcessor(files, FileType.Jpeg, true, true);
+
+        var processedFiles = await processor.ProcessAsync();
         
-        await processor.Process();
-        
-        Assert.Equal(FileStatus.FailedUnknownFormat, processor.FilesStatus.Keys.First());
+        Assert.Equal(FileStatus.FailedUnknownFormat, processedFiles.First().FileStatus);
     }
 
     [Fact]
-    public async void Pdf_files_should_not_compress_or_resize()
+    public async void Invalid_files_should_not_return_the_file_stream()
     {
+        var files = CreateInvalidFile();
+        var processor = new FileProcessor(files, FileType.Jpeg, true, true);
+
+        var processedFiles = await processor.ProcessAsync();
         
+        Assert.Null(processedFiles.First().ConvertedFile);
+    }
+
+    [Fact]
+    public async void Non_supported_target_files_type_should_not_process()
+    {
+        var files = CreateInvalidFile();
+        Assert.ThrowsAny<Exception>( () => new FileProcessor(files, 0, true, true));
+    }
+
+    private Dictionary<MemoryStream, string> CreateInvalidFile()
+    {
+        return new Dictionary<MemoryStream, string> { { new MemoryStream("Test file"u8.ToArray()), "test file name" } };
     }
 }
