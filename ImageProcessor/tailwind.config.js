906ec128457b387df85df11ba8ff469d85266d93
joaoHass <joaopedrohass@outlook.com>
2024-04-25 10:47:15 -0300

    chore add tailwind as dev dependency

---
+++ ImageProcessor/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+       './Presentation/Pages/**/*.cshtml',
+       './Presentation/Views/**/*.cshtml'
+	],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
