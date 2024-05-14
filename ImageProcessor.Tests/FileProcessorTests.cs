87965218fb870f66b39dcd9ebfd0ac0640a4c0ca
joaoHass <joaopedrohass@outlook.com>
2024-05-13 21:22:07 -0300

    test adequate tests to changes caused by the new file storage logic

--- ImageProcessor.Tests/FileProcessorTests.cs
+++ ImageProcessor.Tests/FileProcessorTests.cs
@@ -1,11 +1,13 @@
 using System.Net;
 using ImageProcessor.Data;
 using ImageProcessor.Domain;
+using ImageProcessor.Domain.FileStorageStrategy;
 using ImageProcessor.Domain.Models;
 using ImageProcessor.Presentation.Controllers;
 using ImageProcessor.Presentation.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Xunit.Abstractions;
 
 namespace ImageProcessor.Tests;
@@ -15,19 +17,23 @@ public class FileProcessorTests
     private readonly string _filesPath;
     private readonly ITestOutputHelper _testOutputHelper;
     private readonly ApplicationDbContext _context;
+    private readonly IFileStorageStrategy _fileStorage;
+    private readonly ILogger<FileController> _logger;
 
     public FileProcessorTests(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
         _filesPath = "C:\\Users\\Hass\\Documents\\file-processor\\ImageProcessor.Tests\\TestFiles";
         _context = new DbContextFactory().Create();
+        _fileStorage = new LocalFileStorageStrategy();
+        _logger = _testOutputHelper.BuildLoggerFor<FileController>();
     }
 
     [Fact]
     public async void Valid_files_should_be_processed_successfully()
     {
         var files = CreateValidFile();
-        var processorFactory = new FileProcessorFactory(_context);
+        var processorFactory = new FileProcessorFactory(_context, _fileStorage);
         var processor = processorFactory.Create(files, FileType.Jpeg, true, true);
 
         var processedFiles = await processor.ProcessAsync();
@@ -41,7 +47,7 @@ public class FileProcessorTests
     public async void Invalid_files_should_be_processed_as_failed()
     {
         var files = CreateInvalidFile();
-        var processorFactory = new FileProcessorFactory(_context);
+        var processorFactory = new FileProcessorFactory(_context, _fileStorage);
         var processor = processorFactory.Create(files, FileType.Jpeg, true, true);
 
         var processedFiles = await processor.ProcessAsync();
@@ -55,7 +61,7 @@ public class FileProcessorTests
     public async void Failed_files_should_return_null_for_the_resulting_stream()
     {
         var files = CreateInvalidFile();
-        var processor = new FileProcessorFactory(_context).Create(files, FileType.Jpeg, true, true);
+        var processor = new FileProcessorFactory(_context, _fileStorage).Create(files, FileType.Jpeg, true, true);
 
         var processedFiles = await processor.ProcessAsync();
 
@@ -65,7 +71,7 @@ public class FileProcessorTests
     [Fact]
     public async void UploadFiles_should_reject_requests_when_files_property_is_null()
     {
-        var controller = new FileController(new FileProcessorFactory(_context));
+        var controller = new FileController(new FileProcessorFactory(_context, _fileStorage), _logger);
 
         var result = await controller.UploadFiles(new FilesUploadDto(null, FileType.Jpeg, true, true));
         var statusResult = result as ObjectResult;
@@ -77,7 +83,7 @@ public class FileProcessorTests
     [Fact]
     public async void UploadFiles_should_reject_requests_that_contains_files_larger_than_5mb()
     {
-        var controller = new FileController(new FileProcessorFactory(_context));
+        var controller = new FileController(new FileProcessorFactory(_context, _fileStorage), _logger);
         var ms = CreateValidFile().First().Key;
         ms.SetLength(5242880 + 1);
         IFormFile[] formFiles = [new FormFile(ms, 0, ms.Length, "valid_test_file.png", "valid_test_file.png")];
@@ -92,7 +98,7 @@ public class FileProcessorTests
     [Fact]
     public async void UploadFiles_should_reject_requests_with_more_than_10_files()
     {
-        var controller = new FileController(new FileProcessorFactory(_context));
+        var controller = new FileController(new FileProcessorFactory(_context, _fileStorage), _logger);
         var ms = CreateValidFile().First().Key;
         var formFiles = new IFormFile[11];
         for (int i = 0; i < 11; i++)
@@ -111,7 +117,7 @@ public class FileProcessorTests
     public void Non_supported_target_files_type_should_not_process()
     {
         var file = CreateInvalidFile();
-        Assert.ThrowsAny<Exception>( () => new FileProcessorFactory(_context).Create(file, 0, true, true));
+        Assert.ThrowsAny<Exception>( () => new FileProcessorFactory(_context, _fileStorage).Create(file, 0, true, true));
     }
 
     private Dictionary<MemoryStream, string> CreateInvalidFile()
